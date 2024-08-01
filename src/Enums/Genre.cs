c907f3f529c410a9cc6504d51b4fe4ee6f0bbcdd
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-01 18:07:55 -0400

    feat: add class Genre Enumerable

---
+++ src/Enums/Genre.cs
@@ -0,0 +1,25 @@
+﻿namespace Opcion1SaletGutierrez.src.Enums;
+
+public enum Genre
+{
+    Thriller,
+    HistoricalNovel,
+    Romance,
+    ScienceFiction,
+    Dystopia,
+    Adventure,
+    Fantasy,
+    Contemporary,
+    Horror,
+    Paranormal,
+    Poetry,
+    YoungAdult,
+    Children,
+    SelfHelp,
+    HealthAndSports,
+    Memoir,
+    Biography,
+    Cooking,
+    Travel,
+    Art
+}
