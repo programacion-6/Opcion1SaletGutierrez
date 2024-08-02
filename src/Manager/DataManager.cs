40c4010eabdd0cac40bbc0bf321aa6f1d2e298eb
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-02 00:45:56 -0400

    feat: add implementation of the generic IDataManager interface

--- src/Manager/DataManager.cs
+++ src/Manager/DataManager.cs
@@ -1,11 +1,43 @@
-﻿namespace Opcion1SaletGutierrez.src.Manager;
+﻿using Opcion1SaletGutierrez.src.Interfaces;
 
-public class DataManager<T>
+namespace Opcion1SaletGutierrez.src.Manager;
+
+public class DataManager<T> : IDataManager<T>
 {
     private readonly string _jsonFilePath;
 
     public DataManager(string jsonFilePath)
     {
         _jsonFilePath = jsonFilePath;
+    }
+
+    public bool AddItem(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<T> LoadData(string jsonFilePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveItem(string attributeName, object id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<T> SearchByAttribute(string attributeName, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T SearchById(string attributeName, T id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateItem(string attributeName, object id, T updatedItem)
+    {
+        throw new NotImplementedException();
     }
 }
