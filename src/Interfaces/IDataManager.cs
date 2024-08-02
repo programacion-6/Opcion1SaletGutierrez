c1cb1d0f9d751f9fe5ac7b0c5c231b9739efb02f
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-02 00:16:48 -0400

    feat: add IDataManager interface class with necessary methods

---
+++ src/Interfaces/IDataManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Opcion1SaletGutierrez.src.Interfaces;
+
+public interface IDataManager<T>
+{
+    List<T> LoadData(string jsonFilePath);
+
+    T SearchById(string attributeName, T id);
+
+    List<T> SearchByAttribute(string attributeName, string value);
+
+    bool AddItem(T item);
+
+    bool RemoveItem(string attributeName, object id);
+
+    bool UpdateItem(string attributeName, object id, T updatedItem);
+}
