bf83dae8bc884ba9f4ec46a87775c31919a8e6d5
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-02 00:41:27 -0400

    feat: add DataManager class

---
+++ src/Manager/DataManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Opcion1SaletGutierrez.src.Manager;
+
+public class DataManager<T>
+{
+    private readonly string _jsonFilePath;
+
+    public DataManager(string jsonFilePath)
+    {
+        _jsonFilePath = jsonFilePath;
+    }
+}
