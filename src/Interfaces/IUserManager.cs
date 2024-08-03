bb0ce93f17ab18823d2256050cbde59c57b734e1
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-03 12:04:09 -0400

    feat: add IUserManager interface with its methods

---
+++ src/Interfaces/IUserManager.cs
@@ -0,0 +1,18 @@
+﻿using Opcion1SaletGutierrez.src.Models;
+
+namespace Opcion1SaletGutierrez.src.Interfaces;
+
+public interface IUserManager
+{
+    List<User> SearchUserByName(string nameUser);
+
+    User SearchUserByMemberNumber(int memberNumber);
+
+    User SearchUserByContactInfo(string contactInfo);
+
+    bool AddUser(User user);
+
+    bool RemoveUser(int memberNumber);
+
+    bool UpdateUser(int memberNumber, User updatedUser);
+}
