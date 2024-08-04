9715e2ead1b595cb0d6284f2e7331aa3c6ce99dd
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-08-03 21:20:58 -0400

    feat: add BookLoanDate class with the necessary attributes to use in Loan

---
+++ src/Models/BookLoanDate.cs
@@ -0,0 +1,10 @@
+﻿namespace Opcion1SaletGutierrez.src.Models;
+
+public class BookLoan
+{
+    public Book BookBorrow { get; set; }
+
+    public DateTime BorrowDate { get; set; }
+
+    public DateTime BorrowDueDate { get; set; }
+}
